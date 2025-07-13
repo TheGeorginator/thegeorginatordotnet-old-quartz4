49bef18a2d8b6bcef64e0f7cf104a5901fc58616
George Baker <gabaker256@gmail.com>
2025-07-12 23:58:18 -0500

    Quartz sync: Jul 12, 2025, 11:58 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -29,14 +29,14 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+          light: "#FFF0F7",
+          lightgray: "#e07994ff",
+          gray: "#d168b3ff",
+          darkgray: "#000000ff",
+          dark: "#9200ccff",
+          secondary: "#e100ffff",
+          tertiary: "#4a004dff",
+          highlight: "rgba(0, 238, 255, 0.15)",
           textHighlight: "#fff23688",
         },
         darkMode: {
