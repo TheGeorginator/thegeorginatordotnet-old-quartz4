f8e0a7f9295e3d887f0137991d33d738c3e6e055
George Baker <gabaker256@gmail.com>
2025-07-12 21:52:21 -0500

    Quartz sync: Jul 12, 2025, 9:52 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "TheGeorginator.net",
-    pageTitleSuffix: "",
+    pageTitle: "TG's Site!",
+    pageTitleSuffix: "Wow for sure!",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
